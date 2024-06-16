04e82b5d059cbde89b917b927af2e50bf689fea7
the-layman <sahinvictor@gmail.com>
2024-06-15 20:55:46 -0700

    Start implementing patient level training

--- rsna-2024/train_cnn_lstm.py
+++ rsna-2024/train_cnn_lstm.py
@@ -62,6 +62,44 @@ class CustomLSTM(nn.Module):
         return x
 
 
+class CustomTriLSTM(nn.Module):
+    hidden_size = 256
+    num_layers = 2
+
+    def __init__(self, num_classes=3, drop_rate=0.2, resnet_weights=None):
+        super(CustomTriLSTM, self).__init__()
+        self.cnn = CustomResNet(pretrained_weights=resnet_weights)
+        self.lstm_t1 = nn.LSTM(input_size=512, hidden_size=self.hidden_size, num_layers=self.num_layers, batch_first=True, bidirectional=True)
+        self.lstm_t2 = nn.LSTM(input_size=512, hidden_size=self.hidden_size, num_layers=self.num_layers, batch_first=True, bidirectional=True)
+        self.lstm_t2stir = nn.LSTM(input_size=512, hidden_size=self.hidden_size, num_layers=self.num_layers, batch_first=True, bidirectional=True)
+        self.head = nn.Sequential(
+            nn.Linear(256, 128),
+            # nn.BatchNorm1d(256),
+            nn.Dropout(drop_rate),
+            nn.LeakyReLU(0.1),
+            nn.Linear(128, num_classes),
+        )
+    def forward(self, x_3d, series_label):
+        hidden = None
+
+        # Iterate over each frame of a video in a video of batch * frames * channels * height * width
+        for t in range(x_3d.size(1)):
+            with torch.no_grad():
+                x = self.cnn(x_3d[:, t])
+                # Pass latent representation of frame through lstm and update hidden state
+            if series_label == "t1":
+                out, hidden = self.lstm_t1(x.unsqueeze(0), hidden)
+            elif series_label == "t2":
+                out, hidden = self.lstm_t2(x.unsqueeze(0), hidden)
+            elif series_label == "t2stir":
+                out, hidden = self.lstm_t2stir(x.unsqueeze(0), hidden)
+
+            # Get the last hidden state (hidden is a tuple with both hidden and cell state in it)
+        x = self.head(hidden[0][-1])
+
+        return x
+
+
 def freeze_model_initial_layers(model: CustomLSTM):
     for param in model.cnn.model.parameters():
         param.requires_grad = False
@@ -206,6 +244,10 @@ def train_model_for_series(data_subset_label: str, model_label: str):
                                 epochs=NUM_EPOCHS)
 
     return model
+
+
+def train_model_patient_level():
+    raise NotImplementedError()
 
 
 def train():
