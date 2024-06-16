337c0655912a6a9b0083c8230ecf3814fb03366a
the-layman <sahinvictor@gmail.com>
2024-06-15 18:02:12 -0700

    Use dropout and leaky relu at head

--- rsna-2024/train_cnn_lstm.py
+++ rsna-2024/train_cnn_lstm.py
@@ -34,16 +34,17 @@ class CustomLSTM(nn.Module):
     hidden_size = 256
     num_layers = 3
 
-    def __init__(self, num_classes=3, resnet_weights=None):
+    def __init__(self, num_classes=3, drop_rate=0.3, resnet_weights=None):
         super(CustomLSTM, self).__init__()
         self.resnet = CustomResNet(pretrained_weights=resnet_weights)
         self.lstm = nn.LSTM(input_size=512, hidden_size=self.hidden_size, num_layers=self.num_layers, batch_first=True)
-        self.tail = nn.Sequential(
-            nn.Linear(self.hidden_size, 128),
-            nn.ReLU(),
-            nn.Linear(128, num_classes)
+        self.head = nn.Sequential(
+            nn.Linear(512, 256),
+            nn.BatchNorm1d(256),
+            nn.Dropout(drop_rate),
+            nn.LeakyReLU(0.1),
+            nn.Linear(256, num_classes),
         )
-
     def forward(self, x_3d):
         hidden = None
 
@@ -55,7 +56,7 @@ class CustomLSTM(nn.Module):
             out, hidden = self.lstm(x.unsqueeze(0), hidden)
 
             # Get the last hidden state (hidden is a tuple with both hidden and cell state in it)
-        x = self.tail(hidden[0][-1])
+        x = self.head(hidden[0][-1])
 
         return x
 
