418987d6b0a50124a19320bd9698b240c3707a74
the-layman <sahinvictor@gmail.com>
2024-06-25 01:13:01 -0700

    Cache purge every 20 iters instead of 10

--- rsna-2024/training_utils.py
+++ rsna-2024/training_utils.py
@@ -142,7 +142,7 @@ def train_model_with_validation(model, optimizers, schedulers, loss_fns, train_l
                 optimizer.step()
 
             #prof.step()
-            if index % 10 == 0:
+            if index % 20 == 0:
                 torch.cuda.empty_cache()
 
         epoch_loss = epoch_loss / len(train_loader)
