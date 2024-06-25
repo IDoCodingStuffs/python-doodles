50fc3a8eab0f1a896d4027d2e8be84a9e1318014
the-layman <sahinvictor@gmail.com>
2024-06-24 22:59:14 -0700

    Purge cache only every 10 iters to gain a bit of speed

--- rsna-2024/training_utils.py
+++ rsna-2024/training_utils.py
@@ -142,7 +142,8 @@ def train_model_with_validation(model, optimizers, schedulers, loss_fns, train_l
                 optimizer.step()
 
             #prof.step()
-            torch.cuda.empty_cache()
+            if index % 10 == 0:
+                torch.cuda.empty_cache()
 
         epoch_loss = epoch_loss / len(train_loader)
         epoch_validation_loss = model_validation_loss(model, val_loader, loss_fns, epoch)
