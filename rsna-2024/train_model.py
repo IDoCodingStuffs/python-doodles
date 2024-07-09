03855d94ed00875f3edbf669969f6056900e2ce7
the-layman <sahinvictor@gmail.com>
2024-07-08 18:39:15 -0700

    Altenative calculation for pos weights

--- rsna-2024/train_model.py
+++ rsna-2024/train_model.py
@@ -36,32 +36,61 @@ CONFIG = dict(
 DATA_BASEPATH = "./data/rsna-2024-lumbar-spine-degenerative-classification/"
 TRAINING_DATA = retrieve_coordinate_training_data(DATA_BASEPATH)
 
-CLASS_WEIGHTS = torch.Tensor([1., 29.34146341, 601.5,
-                 1., 10.46296296, 141.25,
-                 1., 3.6539924, 43.68181818,
-                 1., 1.89223058, 8.20652174,
-                 1., 2.31736527, 5.60869565,
-                 1., 19.46666667, 64.88888889,
-                 1., 6.30674847, 18.69090909,
-                 1., 2.92041522, 7.46902655,
-                 1., 1.5144357, 2.00347222,
-                 1., 3.43076923, 9.4893617,
-                 1., 27.11363636, 132.55555556,
-                 1., 10.5, 283.5,
-                 1., 3.65267176, 35.44444444,
-                 1., 2.05277045, 8.74157303,
-                 1., 2.75333333, 6.88333333,
-                 1., 14.59493671, 82.35714286,
-                 1., 6.32926829, 23.59090909,
-                 1., 2.82828283, 7.70642202,
-                 1., 1.43367347, 1.92465753,
-                 1., 3.57429719, 8.31775701,
-                 1., 29.04878049, 85.07142857,
-                 1., 11.31632653, 28.43589744,
-                 1., 7.16083916, 12.96202532,
-                 1., 6.25675676, 5.38372093,
-                 1., 44.66666667, 92.76923077
-                 ]).to(CONFIG["device"])
+CLASS_RELATIVE_WEIGHTS = torch.Tensor([1., 29.34146341, 601.5,
+                                       1., 10.46296296, 141.25,
+                                       1., 3.6539924, 43.68181818,
+                                       1., 1.89223058, 8.20652174,
+                                       1., 2.31736527, 5.60869565,
+                                       1., 19.46666667, 64.88888889,
+                                       1., 6.30674847, 18.69090909,
+                                       1., 2.92041522, 7.46902655,
+                                       1., 1.5144357, 2.00347222,
+                                       1., 3.43076923, 9.4893617,
+                                       1., 27.11363636, 132.55555556,
+                                       1., 10.5, 283.5,
+                                       1., 3.65267176, 35.44444444,
+                                       1., 2.05277045, 8.74157303,
+                                       1., 2.75333333, 6.88333333,
+                                       1., 14.59493671, 82.35714286,
+                                       1., 6.32926829, 23.59090909,
+                                       1., 2.82828283, 7.70642202,
+                                       1., 1.43367347, 1.92465753,
+                                       1., 3.57429719, 8.31775701,
+                                       1., 29.04878049, 85.07142857,
+                                       1., 11.31632653, 28.43589744,
+                                       1., 7.16083916, 12.96202532,
+                                       1., 6.25675676, 5.38372093,
+                                       1., 44.66666667, 92.76923077
+                                       ]).to(CONFIG["device"])
+
+CLASS_NEG_VS_POS = torch.Tensor(
+    [3.57439734e-02, 2.93902439e+01, 6.22000000e+02,
+     1.02654867e-01, 1.05370370e+01, 1.54750000e+02,
+     0.29656608, 3.73764259, 55.63636364,
+     0.65033113, 2.12280702, 12.54347826,
+     0.60981912, 2.73053892, 8.02898551,
+     6.67808219e-02, 1.97666667e+01, 6.82222222e+01,
+     0.21206226, 6.64417178, 21.65454545,
+     0.47630332, 3.31141869, 10.02654867,
+     1.15944541, 2.27034121, 3.32638889,
+     0.39686099, 3.79230769, 12.25531915,
+     4.44258173e-02, 2.73181818e+01, 1.37444444e+02,
+     9.87654321e-02, 1.05370370e+01, 3.10500000e+02,
+     0.30198537, 3.75572519, 45.14814815,
+     0.60154242, 2.28759894, 13.,
+     0.50847458, 3.15333333, 9.38333333,
+     8.06591500e-02, 1.47721519e+01, 8.80000000e+01,
+     0.20038536, 6.59756098, 27.31818182,
+     0.48333333, 3.1952862, 10.43119266,
+     1.21708185, 2.17857143, 3.26712329,
+     0.4, 4.00401606, 10.64485981,
+     4.61796809e-02, 2.93902439e+01, 8.80000000e+01,
+     0.12353472, 11.71428571, 30.94871795,
+     0.21679688, 7.71328671, 14.7721519,
+     0.34557235, 7.41891892, 6.24418605,
+     3.31674959e-02, 4.51481481e+01, 9.48461538e+01]
+).to(CONFIG["device"])
+
 
 class NormMLPClassifierHead(nn.Module):
     def __init__(self, in_dim, out_dim):
@@ -196,7 +225,7 @@ class CNN_Transformer_Model(nn.Module):
         feat = self.pos_emb(feat)
         feat = self.attention_layer(feat)
         feat = self.avg_pool(feat)
-        #feat = self.head(feat[:, 0])
+        # feat = self.head(feat[:, 0])
         feat = self.head(feat)
 
         return feat.reshape((-1, CONFIG["n_levels"] * self.handedness_factor, CONFIG["out_dim"]))
@@ -234,11 +263,10 @@ class CNN_Transformer_Model_Multichannel(nn.Module):
         self.head = NormMLPClassifierHead(in_dim=hdim,
                                           out_dim=self.num_levels * CONFIG["out_dim"])
 
-
     def forward(self, x):
         feat = self.encoder(x)
         feat = self.attention_layer(feat)
-        #feat = self.head(feat[:, 0])
+        # feat = self.head(feat[:, 0])
         feat = self.head(feat)
 
         return feat.reshape((-1, self.num_levels, CONFIG["out_dim"]))
@@ -433,8 +461,8 @@ def train_model_3d(backbone, model_label: str):
                                                                             transform_val,
                                                                             transform_3d_train=transform_3d_train,
                                                                             base_path=os.path.join(
-                                                                               DATA_BASEPATH,
-                                                                               "train_images"),
+                                                                                DATA_BASEPATH,
+                                                                                "train_images"),
                                                                             num_workers=22,
                                                                             split_factor=0.3,
                                                                             batch_size=8,
@@ -450,7 +478,7 @@ def train_model_3d(backbone, model_label: str):
     ]
     criteria = [
         # WeightedBCELoss(device=CONFIG["device"])
-        nn.BCEWithLogitsLoss(pos_weight=CLASS_WEIGHTS)
+        nn.BCEWithLogitsLoss(pos_weight=CLASS_NEG_VS_POS)
     ]
 
     train_model_with_validation(model,
@@ -470,7 +498,8 @@ def train_model_3d(backbone, model_label: str):
 
 def train():
     # model_t2stir = train_model_for_series("Sagittal T2/STIR", "efficientnet_b4_multichannel_shuffled_t2stir")
-    model = train_model_3d("efficientnet_b0", f"{CONFIG['backbone']}_{CONFIG['img_size'][0]}_3d_weighted_bce")
+    model = train_model_3d("efficientnet_b0",
+                           f"{CONFIG['backbone']}_{CONFIG['img_size'][0]}_3d_neg_vs_pos_weighted_bce")
     # model2 = train_model_3d("efficientnet_b3", f"efficientnet_b3_{CONFIG['img_size'][0]}_3d_padded")
     # model_t2 = train_model_3d("Axial T2", "efficientnet_b0_3d_t2")
 
