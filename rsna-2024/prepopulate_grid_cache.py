7766a1737ee51f145c2c0622d2082b2f0bda88c7
the-layman <sahinvictor@gmail.com>
2024-08-06 21:44:12 -0700

    Update cache prepop script

--- rsna-2024/prepopulate_grid_cache.py
+++ rsna-2024/prepopulate_grid_cache.py
@@ -1,99 +1,31 @@
 import math
-
-import open3d as o3d
-import pydicom
-import numpy as np
-import glob
-import os
-import pgzip
 from tqdm import tqdm
 import argparse
 
-def read_series_as_pcd(dir_path):
-    pcds_xyz = []
-    pcds_d = []
+from rsna_dataloader import *
 
-    paths = glob.glob(os.path.join(dir_path, "*.dcm"))
-    for path in paths:
-        dicom_slice = pydicom.read_file(path)
-        img = np.expand_dims(dicom_slice.pixel_array, -1)
-        pcd = o3d.geometry.PointCloud()
-        x, y, z = np.where(img)
+DATA_BASEPATH = "./data/rsna-2024-lumbar-spine-degenerative-classification/"
+TRAINING_DATA = retrieve_coordinate_training_data(DATA_BASEPATH)
 
-        index_voxel = np.vstack((x, y, z))
-        grid_index_array = index_voxel.T
-        pcd.points = o3d.utility.Vector3dVector(grid_index_array)
+df = TRAINING_DATA.dropna()
+# This drops any subjects with nans
 
-        vals = np.array([img[x, y, z] for x, y, z in grid_index_array])
+filtered_df = pd.DataFrame(columns=df.columns)
+for series_desc in CONDITIONS.keys():
+    subset = df[df['series_description'] == series_desc]
+    if series_desc == "Sagittal T2/STIR":
+        subset = subset[subset.groupby(["study_id"]).transform('size') == 5]
+    else:
+        subset = subset[subset.groupby(["study_id"]).transform('size') == 10]
+    filtered_df = pd.concat([filtered_df, subset])
 
-        dX, dY = dicom_slice.PixelSpacing
-        X = np.array(list(dicom_slice.ImageOrientationPatient[:3]) + [0]) * dX
-        Y = np.array(list(dicom_slice.ImageOrientationPatient[3:]) + [0]) * dY
-        S = np.array(list(dicom_slice.ImagePositionPatient) + [1])
+filtered_df = filtered_df[filtered_df.groupby(["study_id"]).transform('size') == 25]
 
-        transform_matrix = np.array([X, Y, np.zeros(len(X)), S]).T
-        transformed_pcd = pcd.transform(transform_matrix)
+series_descs = {e[0]: e[1] for e in df[["series_id", "series_description"]].drop_duplicates().values}
 
-        pcds_xyz.extend(transformed_pcd.points)
-        pcds_d.extend(vals)
-
-        pcd.clear()
-        transformed_pcd.clear()
-
-    pcd_xyzd = np.hstack((pcds_xyz, np.expand_dims(pcds_d, -1)))
-
-    return pcd_xyzd
-
-
-def read_series_as_voxel_grid(dir_path):
-    cache_path = os.path.join(dir_path, "cached_grid.npy.gz")
-    f = None
-    if os.path.exists(cache_path):
-        try:
-            f = pgzip.PgzipFile(cache_path, "r")
-            ret = np.load(f)
-            f.close()
-            return ret
-        except Exception as e:
-            print(dir_path, "\n", e)
-            if f:
-                f.close()
-            os.remove(cache_path)
-
-    pcd_xyzd = read_series_as_pcd(dir_path)
-
-    pcd_overall = o3d.geometry.PointCloud()
-    pcd_overall.points = o3d.utility.Vector3dVector(pcd_xyzd[:, :3])
-
-    paths = glob.glob(os.path.join(dir_path, "*.dcm"))
-    dicom_slice = pydicom.read_file(paths[0])
-    dX, dY = dicom_slice.PixelSpacing
-
-    voxel_grid = o3d.geometry.VoxelGrid().create_from_point_cloud(pcd_overall, dX)
-
-    coords = np.array([voxel.grid_index for voxel in voxel_grid.get_voxels()])
-    vals = pcd_xyzd[:, 3]
-
-    pcd_overall.clear()
-    voxel_grid.clear()
-
-    size = np.max(coords, axis=0) + 1
-    grid = np.zeros((size[1], size[0], size[2]))
-
-    for index, coord in enumerate(coords):
-        grid[(coord[1], coord[0], coord[2])] = vals[index]
-
-    f = pgzip.PgzipFile(cache_path, "w")
-    np.save(f, grid)
-    f.close()
-
-    del pcd_overall
-    del voxel_grid
-
-    return grid
 
 if __name__ == "__main__":
-    dirs = glob.glob("./data/rsna-2024-lumbar-spine-degenerative-classification/train_images/*/*/")
+    dirs = glob.glob("./data/rsna-2024-lumbar-spine-degenerative-classification/train_images/*/")
     dirs = sorted(dirs)
 
     parser = argparse.ArgumentParser(
@@ -107,4 +39,4 @@ if __name__ == "__main__":
     dirslice = dirs[slice_size*args.index:slice_size*(args.index + 1)]
 
     for dir in tqdm(dirslice):
-        read_series_as_voxel_grid(dir)
+        read_study_as_voxel_grid(dir, series_type_dict=series_descs)
